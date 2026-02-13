dc6ab25ad6c57afcda8f85a876ea311313447d1c
youduo <youduo.blvms.945@gmail.com>
2026-02-13 16:00:47 +0900

    validatesの修正と追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   has_many :prototypes
   has_many :comments, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, presence: true
+
   validates :name, presence: true
-  validates :encrypted_password, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+  
 end
